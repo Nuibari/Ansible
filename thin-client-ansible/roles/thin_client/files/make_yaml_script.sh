7ebc1b4a065c3bd6bc4f6eeca2bcf17cce91f1d9
Diana <46224962+Nuibari@users.noreply.github.com>
2021-01-20 09:40:48 +0100

    Add files via upload

---
+++ thin-client-ansible/roles/thin_client/files/make_yaml_script.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage:\n$0 <in_path> <out_file>"
+fi
+
+IN_PATH="$1"
+OUT_FILE="$2"
+
+# dla każdego pliku w lokalizacji uruchomienia skryptu 
+for mac in "$IN_PATH/"*.xml; do
+    macdir=`dirname "$mac"`
+
+    # MAC wskasuje na name, więc pobieramy treść linka mac
+    name=`readlink "$mac"`
+    name="$macdir/$name"
+    namedir=`dirname "$name"`
+    name=`basename "$name"`
+
+    # name wskazuje na template, więc powieramy treść linka name
+    template=`readlink "$namedir/$name"`
+
+    mac=`basename "$mac"`
+    template=`basename "$template"`
+
+    printf -- "-\n  mac: \"%s\"\n  name: \"%s\"\n  template: \"%s\"\n" "$mac" "$name" "$template" >> "$OUT_FILE"
+done
